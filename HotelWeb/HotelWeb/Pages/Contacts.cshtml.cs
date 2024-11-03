3b5636bd62494d4fd6bc7b3d6102e8aee1fb13ed
Артем <artemus200918@gmail.com>
2024-11-03 14:24:53 +0200

    Add file

---
+++ HotelWeb/HotelWeb/Pages/Contacts.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelWeb.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HotelWeb/HotelWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Додатковий код для ініціалізації, якщо необхідно
+        }
+    }
+}
