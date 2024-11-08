76c68beb455d979bdfa013cbd98568b014846894
Артем <artemus200918@gmail.com>
2024-11-08 22:05:05 +0200

    finish

--- HotelWeb/HotelWeb/obj/Debug/net8.0/HotelWeb.AssemblyInfo.cs
+++ HotelWeb/HotelWeb/obj/Debug/net8.0/HotelWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d528e3be42ee62628cba132cef038d6f8ff9db7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4a7dc6d8435e419153a1d859f5555cfa56577e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
